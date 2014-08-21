a09701e385297079ec8e166f035aeeeb53b5541d
weiminw <weiminw80105@gmail.com>
2014-08-22 00:33:58 +0800

    修改图片目录

--- admin/service/src/main/java/com/itravel/admin/services/rest/ImageResource.java
+++ admin/service/src/main/java/com/itravel/admin/services/rest/ImageResource.java
@@ -27,8 +27,18 @@ import com.google.common.io.Files;
 
 @Path("/images")
 public class ImageResource {
-	private static final File IMAGE_DIR=new File("/usr/share/nginx/www/images/");
-	private static final File IMAGE_TEMP_DIR=new File("/tmp");
+	private static File IMAGE_DIR=null;
+	static {
+		if(System.getProperty("imageDir")!=null){
+			IMAGE_DIR=new File(System.getProperty("imageDir"));
+		}
+		else {
+			IMAGE_DIR=new File("/usr/share/nginx/www/images/");//服务器上默认图片目录
+		}
+		if(!IMAGE_DIR.exists()){
+			IMAGE_DIR.mkdirs();
+		}
+	}
 	
 	@Context
 	UriInfo uriInfo;
@@ -44,15 +54,13 @@ public class ImageResource {
 		List<FormDataBodyPart> bodyPartList = formDataMultiPart
 				.getFields("images");
 		FormDataBodyPart part = bodyPartList.get(0);
-		if(!IMAGE_DIR.exists()){
-			IMAGE_DIR.mkdirs();
-		}
-		File _temp = File.createTempFile("activities-", ".png",IMAGE_TEMP_DIR);
-		ByteSink bs = Files.asByteSink(_temp);
+		
+		File imageFile = File.createTempFile("activities-", ".png",IMAGE_DIR);
+		ByteSink bs = Files.asByteSink(imageFile);
 		InputStream input = part.getEntityAs(InputStream.class);
 		bs.writeFrom(input);
 		Map<String,String> entity = Maps.newHashMapWithExpectedSize(1);
-		entity.put("imageNames", _temp.getName());
-		return Response.created(uriInfo.getBaseUriBuilder().path(_temp.getName()).build()).entity(mapper.writeValueAsString(entity)).build();
+		entity.put("imageNames", imageFile.getName());
+		return Response.created(uriInfo.getBaseUriBuilder().path(imageFile.getName()).build()).entity(mapper.writeValueAsString(entity)).build();
 	}
 }
